6fb394c6d34fe697f81a2574bfc26652acb4cef3
AtamanovPetr <peteratamanov@yandex.ru>
2026-06-19 21:41:47 +0300

    fix adaptiv

--- js/main.js
+++ js/main.js
@@ -4,4 +4,9 @@ const mobileMenu = document.getElementById("mobileMenu");
 burgerBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
   burgerBtn.classList.toggle("open");
+  if (mobileMenu.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
 });
