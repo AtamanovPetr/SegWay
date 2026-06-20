c2cacc969b51d2005c4af30b9186d813b40d01b2
AtamanovPetr <peteratamanov@yandex.ru>
2026-06-20 14:20:24 +0300

    add sections

--- js/main.js
+++ js/main.js
@@ -10,3 +10,21 @@ burgerBtn.addEventListener("click", () => {
     document.body.style.overflow = "";
   }
 });
+
+function openTab(evt, tabId) {
+  // Скрываем весь контент
+  const tabContents = document.getElementsByClassName("tab-content");
+  for (let i = 0; i < tabContents.length; i++) {
+    tabContents[i].classList.remove("active");
+  }
+
+  // Убираем активный класс у всех кнопок
+  const tabBtns = document.getElementsByClassName("tab-btn");
+  for (let i = 0; i < tabBtns.length; i++) {
+    tabBtns[i].classList.remove("active");
+  }
+
+  // Показываем текущий контент и делаем кнопку активной
+  document.getElementById(tabId).classList.add("active");
+  evt.currentTarget.classList.add("active");
+}
